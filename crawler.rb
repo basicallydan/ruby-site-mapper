10cf9c1072d9de22a2f00a4dc5eebaea165bd0e3
Dan Hough <daniel.hough@gmail.com>
2013-09-21 20:33:50 +0100

    Extracts images now

--- crawler.rb
+++ crawler.rb
@@ -4,12 +4,23 @@ require 'uri'
 class Crawler
 	def extract_links_from_html(html_string)
 		links = Array.new
-		link_match_regex = Regexp.new("<a href=[\"']?(((https?)?(:\/\/)?([a-zA-Z0-9]+?\.)?gocardless\.com).*?|(?!https?)(?!:\/\/)[[a-zA-Z0-9]\/#\?].+?)[\"']?>", Regexp::IGNORECASE)
+		link_match_regex = Regexp.new("<a href=[\"']?(((https?)?(:\/\/)?([a-zA-Z0-9]+?\.)?gocardless\.com)[^\"'>]*|(?!https?)(?!:\/\/)[[a-zA-Z0-9]\/#\?][^\"'>]*)[\"']?[^>]*?>", Regexp::IGNORECASE)
 		html_string.scan(link_match_regex) do |link|
 			links.push(link[0])
 		end
 
 		links
+	end
+
+	def extract_static_assets_from_html(html_string)
+		assets = Array.new
+		asset_match_regex = Regexp.new("<img src=[\"']?((((http)s?)?://)?[^\" '>]*)[\"']?.*?>", Regexp::IGNORECASE)
+		html_string.scan(asset_match_regex) do |asset|
+			asset[0].strip!
+			assets.push(asset[0])
+		end
+
+		assets
 	end
 
 	def get_page_links(uri_string)
--- spec/crawler_spec.rb
+++ spec/crawler_spec.rb
@@ -4,7 +4,7 @@ require_relative '../crawler'
 describe Crawler, "#extract_links_from_html" do
 	it "returns a list of links" do
 		crawler = Crawler.new
-		links = crawler.extract_links_from_html("<a href=""https://gocardless.com/blog"">blog</a> <a href=""/contact/"">contact</a> <a href=""://gocardless.com/glob"">glob</a> <a href=""https://www.gocardless.com/bleepbloop"">bleep</a> <a href=https://whatever.gocardless.com/stuff>whatever</a> <a href=""http://google.com/privacy"">google privacy</a> <a href=""relative"">hello</a> <a href=""#hash"">hash</a> <a href=""?query=whatever"">query</a>")
+		links = crawler.extract_links_from_html("<a href=\"https://gocardless.com/blog\">blog</a> <a href=\"/contact/\">contact</a> <a href=\"://gocardless.com/glob\">glob</a> <a href=\"https://www.gocardless.com/bleepbloop\" class=\"whee\">bleep</a> <a href=https://whatever.gocardless.com/stuff>whatever</a> <a href=\"http://google.com/privacy\">google privacy</a> <a href=\"relative\">hello</a> <a href=\"#hash\">hash</a> <a href=\"?query=whatever\">query</a>")
 		links.should include("https://gocardless.com/blog")
 		links.should include("/contact/")
 		links.should include("://gocardless.com/glob")
@@ -25,5 +25,19 @@ describe Crawler, "#get_page_links" do
 		links.should include("https://gocardless.com/blog")
 		# puts links
 		links.length.should be > 1
+	end
+end
+
+describe Crawler, "#extract_static_assets_from_html" do
+	it "returns images" do
+		crawler = Crawler.new
+		static_assets = crawler.extract_static_assets_from_html("<img src=cat.gif /> <img src=anothercat.gif> <img src=\"manycats.gif\"> <img src=\"http://gocardless.com/morecats.gif\" class=\"image\"> <img href=dog.png>")
+		static_assets.should include("cat.gif")
+		static_assets.should include("anothercat.gif")
+		static_assets.should include("manycats.gif")
+		static_assets.should include("http://gocardless.com/morecats.gif")
+		static_assets.should_not include("dog.png")
+
+		static_assets.length.should equal(4)
 	end
 end
